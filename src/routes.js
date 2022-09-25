e7bb3237bba02aaa58ac07e52e89fda6438335eb
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-09-25 19:00:14 -0300

    Usando react-router-dom

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function AppRouter() {
+
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Inicio/>}></Route>
+				<Route path='/cardapio' element={<Cardapio/>}></Route>
+			</Routes>
+		</Router>
+	);
+
+}
