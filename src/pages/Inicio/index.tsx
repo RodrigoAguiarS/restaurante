bbcab3e467320ae94ddfbfa3c9189a1c6199370b
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-09-26 22:08:44 -0300

    Terminando a tela Sobre

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,6 +1,7 @@
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
 	let pratosRecomendados  = [...cardapio];
@@ -23,6 +24,13 @@ export default function Inicio() {
 						</button>
 					</div>
 				))} 
+			</div>
+			<h3 className={stylesTema.titulo}> Nossa casa</h3>
+			<div className={styles.nossaCasa}>
+				<img src={nossaCasa} alt="Casa da Massa"/>
+				<div className={styles.nossaCasa__endereco}>
+					Avenida Mar do Norte, 108 <br /> <br/> Natal - RN
+				</div>
 			</div>
 		</section>
 	);
