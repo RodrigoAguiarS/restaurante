8977774c3aca0346b150cecb016292f0b66267b2
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-09-27 09:18:14 -0300

    Obtendo parâmetros de rota e refatorando tags

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -2,16 +2,21 @@ import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
 import nossaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
+import { Prato } from 'types/Prato';
 
 export default function Inicio() {
-	let pratosRecomendados  = [...cardapio];
+	let pratosRecomendados = [...cardapio];
+	pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
+	const navigate = useNavigate();
 
-	pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
-
+	function redirecionarParaDetalhes(prato: Prato) {
+		navigate(`/prato/${prato.id}`, { state: { prato }, replace: true });
+	}
 	return (
 		<section>
 			<h3 className={stylesTema.titulo}>
-            Recomendações da cozinha
+        Recomendações da cozinha
 			</h3>
 			<div className={styles.recomendados}>
 				{pratosRecomendados.map(item => (
@@ -19,17 +24,20 @@ export default function Inicio() {
 						<div className={styles.recomendado__imagem}>
 							<img src={item.photo} alt={item.title} />
 						</div>
-						<button className={styles.recomendado__botao}>
-                        Ver mais
+						<button
+							className={styles.recomendado__botao}
+							onClick={() => redirecionarParaDetalhes(item)}
+						>
+              Ver mais
 						</button>
 					</div>
-				))} 
+				))}
 			</div>
-			<h3 className={stylesTema.titulo}> Nossa casa</h3>
+			<h3 className={stylesTema.titulo}> Nossa casa </h3>
 			<div className={styles.nossaCasa}>
-				<img src={nossaCasa} alt="Casa da Massa"/>
+				<img src={nossaCasa} alt="Casa do aluroni" />
 				<div className={styles.nossaCasa__endereco}>
-					Avenida Mar do Norte, 108 <br /> <br/> Natal - RN
+          Avenida Mar do Norte, 108 <br /> <br /> Natal RN
 				</div>
 			</div>
 		</section>
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,13 @@ import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
 	return (
-		<main>
+		<main className='container'>
 			<Router>
 				<Menu />
 				<Routes>
@@ -17,10 +18,11 @@ export default function AppRouter() {
 						<Route index element={<Inicio />} />
 						<Route path='cardapio' element={<Cardapio />} />
 						<Route path='sobre' element={<Sobre />} />
-						<Route path='*' element={<NotFound />} />
+						<Route path='prato/:id' element={<Prato />} />
 					</Route>
+					<Route path='*' element={<NotFound />} />
 				</Routes>
-				<Footer/>
+				<Footer />
 			</Router>
 		</main>
 	);
